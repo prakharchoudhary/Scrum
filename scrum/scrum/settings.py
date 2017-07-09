9159d81f40b18bbf89f254f9263aa8f60013549f
prakharchoudhary <prakhar2397@gmail.com>
2017-07-09 17:41:44 +0530

    setup drfdocs

--- scrum/scrum/settings.py
+++ scrum/scrum/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "django_filters",
+    "rest_framework_docs",
     #Internal apps
     "board",
 ]
--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     url(r'^api/token/', obtain_auth_token, name='api-token'),
     url(r'^api/', include(router.urls)),
     url(r'^$', TemplateView.as_view(template_name='board/index.html')),
+    url(r'^docs/', include('rest_framework_docs.urls')),
 ]
