95245c4a77796d82f42379eb53c55cb9d85d9a0c
prakharchoudhary <prakhar2397@gmail.com>
2017-07-13 01:59:58 +0530

    add login view and make the views DRY

--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -1,12 +1,47 @@
 
 (function ($, Backbone, _, app) {
 
-	var HomepageView = Backbone.View.extend({
-		templateName: '#home-template',
+	// var HomepageView = Backbone.View.extend({
+	// 	templateName: '#home-template',
+	// 	initialize: function () {
+	// 		this.template = _.template($(this.templateName).html());
+	// 	},
+	// 	render: function () {
+	// 		var context = this.getContext(),
+	// 			html = this.template(context);
+	// 		this.$el.html(html);
+	// 	},
+	// 	getContext: function () {
+	// 		return {};
+	// 	}
+	// });
+
+	// var LoginView = Backbone.View.extend({
+	// 	id: 'login',
+	// 	templateName: '#login-template',
+	// 	initialize: function () {
+	// 		this.template = _.template($(this.templateName).html());
+	// 	},
+	// 	render: function() {
+	// 		var context = this.getContext(),
+	// 			html = this.template(context);
+	// 		this.$el.html(html);
+	// 	},
+	// 	getContext: function () {
+	// 		return {};
+	// 	}
+	// });
+
+	/* 
+	making it DRY
+	*/
+
+	var TemplateView = Backbone.View.extend({
+		templateName: '',
 		initialize: function () {
 			this.template = _.template($(this.templateName).html());
 		},
-		render: funtion () {
+		render: function() {
 			var context = this.getContext(),
 				html = this.template(context);
 			this.$el.html(html);
@@ -16,5 +51,16 @@
 		}
 	});
 
-	apps.views.HomepageView = HomepageView; 
+	var HomepageView = TemplateView.extend({
+		templateName: '#home-template'
+	});
+
+	var LoginView = TemplateView.extend({
+		id: 'login',
+		templateName: '#login-template'
+	});
+		
+	app.views.HomepageView = HomepageView;
+	app.views.LoginView = LoginView;
+ 
 })(jQuery, Backbone, _, app);	
