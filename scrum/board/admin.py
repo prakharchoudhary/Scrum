31df6e251494cbb60dabb4c5fb724e66c290f67d
prakharchoudhary <prakhar2397@gmail.com>
2018-05-08 23:16:38 +0530

    register models to admin panel; add css for task details

--- scrum/board/admin.py
+++ scrum/board/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin  # noqa F401
+from .models import Sprint, Task
 
 # Register your models here.
+admin.site.register(Sprint)
+admin.site.register(Task)
